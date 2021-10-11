c7b37067791b4a21d2a4b704539839c4018f506d
Matthew Walker-Browning <71651059+wbmatthew@users.noreply.github.com>
2021-10-11 14:00:52 -0400

    Update entrypoint.sh
    so THAT was the issue...

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-robocop --configure missing-doc-suite:severity:i --configure todo-in-comment:severity:i --exclude line-too-long --exclude too-many-calls-in-keyword --exclude too-many-calls-in-test-case --exclude too-long-test-case --exclude too-long-keyword
-
 echo "Robocop scan:"
 exec 5>&1
-res=`{ { robocop; echo $? 1>&4; } 1>&5; } 4>&1`
+res=`{ { robocop --configure missing-doc-suite:severity:i --configure todo-in-comment:severity:i --exclude line-too-long --exclude too-many-calls-in-keyword --exclude too-many-calls-in-test-case --exclude too-long-test-case --exclude too-long-keyword; echo $? 1>&4; } 1>&5; } 4>&1`
 if [ "$res" = "0" ]; then
     echo "Robocop found no problems"
 else
